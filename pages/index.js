36359e290f9961859349fbe75f1400e90a3d0d8c
Abu Bakar <bakara097@gmail.com>
2022-09-26 22:42:06 +0500

    Example updated

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
-import React from "react";
+import { useRef, Suspense } from "react";
+
 import { DefaultXRControllers, ARCanvas, useHitTest } from "@react-three/xr";
-import { Box } from "@react-three/drei";
+import { Box, OrbitControls, useTexture } from "@react-three/drei";
 
-function HitTestExample() {
-  const ref = React.useRef();
+import { useRouter } from "next/router";
 
+function HitTestExample({ image }) {
+  const ref = useRef();
+  const texture = useTexture(image);
   useHitTest((hit) => {
     hit.decompose(
       ref.current.position,
@@ -13,19 +16,39 @@ function HitTestExample() {
     );
   });
 
-  return <Box ref={ref} args={[3, 4, 0.5]} />;
+  return (
+    <group>
+      <Box ref={ref} args={[4, 4.5, 0.1]}>
+        <meshStandardMaterial
+          color="#966F33"
+          metalness={0.5}
+          roughness={0.5}
+          envMapIntensity={2}
+        />
+      </Box>
+      <Box ref={ref} args={[3.5, 4, 0.01]} position={[0, 0, 0.05]}>
+        <meshBasicMaterial map={texture} />
+      </Box>
+    </group>
+  );
 }
 
 export default function App() {
+  const router = useRouter();
+  const id = router.query.id;
+  const imageRef = `https://fypaesthetics.s3.ap-south-1.amazonaws.com/${id}`;
   return (
-    <ARCanvas
-      sessionInit={{ requiredFeatures: ["hit-test"] }}
-      style={{ width: "100vw", height: "100vh" }}
-    >
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <HitTestExample />
-      <DefaultXRControllers />
-    </ARCanvas>
+    <Suspense fallback={"loading..."}>
+      <ARCanvas
+        sessionInit={{ requiredFeatures: ["hit-test"] }}
+        style={{ width: "100vw", height: "100vh" }}
+      >
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[10, 10, 10]} />
+        <HitTestExample image={imageRef} />
+        <OrbitControls />
+        <DefaultXRControllers />
+      </ARCanvas>
+    </Suspense>
   );
 }
